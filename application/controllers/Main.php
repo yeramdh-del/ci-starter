a11a9e4eae88f2a377776a9d280d292c2a5ad558
yeramdh-del <yearm.dh@gmail.com>
2025-10-14 15:31:50 +0900

    chore: 메인 컨트롤러 수정

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,13 +3,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Main extends MY_Controller
 {
+    
+    //생성자
     public function __construct()
     {
         parent::__construct();
     }
 
+    //메인 경로
     public function index()
     {
-        echo 'hello world';
+
+        //로그인시
+        redirect("board");
     }
+    
 }
