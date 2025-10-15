66f5cbb1b4733a469f3d09a44ad82ad401d75297
yeramdh-del <yearm.dh@gmail.com>
2025-10-15 17:13:15 +0900

    refector: 게시글 컨트롤러 쿼리문 모델 분리

--- application/controllers/Board.php
+++ application/controllers/Board.php
@@ -18,46 +18,20 @@ class Board extends MY_Controller
         $limit = $this->input->get('limit') ?: 10;
         $pages = $this->input->get('pages') ?: 1;
         $search = $this->input->get('search') ?:null;
-        
+
+
 
         //FIXME: 게시글 등록/수정/삭제 후 SORTING 및 페이지네이션 코드 수정
-        /* 
-            TODO: 게시글 페이지 - 리스트 출력 쿼리문 조건
-            - 정렬
-                - 작성시간 내림차순 : created_at / DESC
-            - 검색
-                - 조건
-            - 페이지 네이션
-                - OFFSET : LIMIT * page + 1
-                - LIMIT : LIMIT
-        */
+        //모델 불러오기
+        $this->load->model("Board_model");
 
-        //페이지네이션 갯수 확인용
-        $select_total= "
-            SELECT
-                COUNT(idx) AS total
-            from 
-                board
-            ";
-        $select_total_query= $this->db->query($select_total);
-        $total = $select_total_query->row();
+        //게시글 리스트 반환 쿼리
+        $result = $this->Board_model->get_all();
 
 
-        //리스트 출력
-        $select_query= '
-            SELECT
-                board.*,
-                user.name as author
-            from 
-                board
-                    left join user
-                    ON board.user_idx = user.idx
-            ';
-        $query= $this->db->query($select_query);
-        $result = $query->result();
-
-        
-
+        //게시글 전채 갯수 반환 (페이지네이션용)
+        $count = $this->Board_model->get_all_count();
+    
         
         $data["page_title"]='게시판';
         $data['content']= "board/list";
@@ -86,30 +60,26 @@ class Board extends MY_Controller
 
         //세션 고객 idx정보
         $user = $this->session->userdata('user');
+
+        //모델 불러오기
+        $this->load->model("Board_model");
+
+        //게시글 리스트 생성 쿼리
+        $this->Board_model->create($title, $content, $user->idx);
         
-        $insert_board = "
-            INSERT INTO
-                board(title,content,user_idx)
-            VALUES(?,?,?)
-        ";
-        $this->db->query($insert_board,array($title,$content,$user->idx));
+       
         redirect("board");
     }
 
     //NOTE: 게시판 - 수정 페이지
     public function edit($idx){
-             $select_board_one = "
-            SELECT
-                idx,
-                title,
-                content
-            FROM
-                board
-            WHERE
-                idx = ?
-        ";
-        $select_board_one_query = $this->db->query($select_board_one,array($idx));
-        $result = $select_board_one_query->row();
+
+        //모델 불러오기
+        $this->load->model("Board_model");
+
+        //게시글 반환 쿼리
+        $result = $this->Board_model->get_one($idx);
+        
 
         $data["page_title"]='게시판 수정페이지';
         $data['content']= "board/edit";
@@ -126,22 +96,19 @@ class Board extends MY_Controller
 
         //권한 확인
         $user = $this->session->userdata("user");
-        
-        $select_board_one = "
-            SELECT
-                b.idx,
-                b.user_idx
-            FROM
-                board AS b
-            WHERE
-                b.idx = ?
-        ";
-        $select_board_one_query = $this->db->query($select_board_one,array($idx));
-        $board_one = $select_board_one_query->row();
+
+
+        //모델 불러오기
+        $this->load->model("Board_model");
+
+        //게시글 반환 쿼리
+        $board_one = $this->Board_model->get_one($idx);
+
+
 
 
         echo json_encode($board_one);
-        //존재하지 않는 게시글일때 알람 출력
+        // //존재하지 않는 게시글일때 알람 출력
         if(!$board_one){
            echo "
                 <script>
@@ -157,16 +124,8 @@ class Board extends MY_Controller
                 </script>";
         }else{
 
-            $update_board = "
-                UPDATE
-                    board
-                SET
-                    title = ?,
-                    content = ?
-                WHERE
-                    idx = ?
-            ";
-            $this->db->query($update_board,array($title,$content,$idx));
+            //게시글 수정 쿼리
+            $this->Board_model->update($title, $content, $idx);
             redirect("board");
         }
 
@@ -175,21 +134,12 @@ class Board extends MY_Controller
     //NOTE: 게시판 - 상세 페이지
     public function detail($idx){
 
-        $select_board_detail = "
-            SELECT
-                b.idx,
-                b.title,
-                b.content,
-                u.name AS author
-            FROM 
-                board AS b
-                    left join user AS u
-                    ON b.user_idx = u.idx
-            WHERE
-                b.idx = ?                
-        ";
-        $select_board_detail_query = $this->db->query($select_board_detail, array($idx));
-        $result = $select_board_detail_query->row();
+
+        //모델 불러오기
+        $this->load->model("Board_model");
+
+        //게시글 반환 쿼리
+        $result = $this->Board_model->get_one($idx);
 
 
         $data["page_title"]='게시판 상세페이지';
@@ -204,17 +154,11 @@ class Board extends MY_Controller
             //권한 확인
             $user = $this->session->userdata("user");
             
-            $select_board_one = "
-                SELECT
-                    b.idx,
-                    b.user_idx
-                FROM
-                    board AS b
-                WHERE
-                    b.idx = ?
-            ";
-            $select_board_one_query = $this->db->query($select_board_one,array($idx));
-            $board_one = $select_board_one_query->row();
+            //모델 불러오기
+            $this->load->model("Board_model");
+
+            //게시글 반환 쿼리
+            $board_one = $this->Board_model->get_one($idx);
 
             //존재하지 않는 게시글일때 알람 출력
             if(!$board_one){
@@ -231,16 +175,10 @@ class Board extends MY_Controller
                         location.href = '" . base_url('board') . "';
                     </script>";
             }else{
-                $delete_board = "
-                    DELETE FROM board
-                    WHERE idx = ?
-                ";
-                $this->db->query($delete_board,array($idx));
-
+                
+                $this->Board_model->delete($idx);
                 redirect("board");
             }
 
-
-        
         }
 }
---
+++ application/models/Board_model.php
@@ -0,0 +1,125 @@
+<?php
+class Board_model extends MY_Model{
+   
+
+
+    //생성자
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+    //게시글 리스트 전체 검색 매서드
+    public function get_all($search = null, $limit = 10, $pages= 0){
+
+        //리스트 출력
+        //NOTE: 기능정의서 목록 부분
+        //  - 작성시간 내림차순
+        //  - 페이지당 표시될 갯수 지정
+        $select_query= '
+            SELECT
+                board.*,
+                user.name as author
+            from 
+                board
+                    left join user
+                    ON board.user_idx = user.idx
+            ORDER BY
+                created_at DESC
+            LIMIT ?
+            OFFSET ?
+            ';
+        $query= $this->db->query($select_query,array($limit,($pages*$limit)));
+        return $query->result();
+
+    }
+
+
+    //게시글 전채 갯수 검색 매서드
+    public function get_all_count($search=null){
+        $select_query= '
+            SELECT
+                COUNT(idx) AS total
+            from 
+                board
+            ';
+        $query= $this->db->query($select_query);
+        return $query->row();
+    }
+
+    //게시글 등록 매서드
+    public function create($title,$content,$user_idx){
+
+        //  $insert_board = "
+        //     INSERT INTO
+        //         board(title,content,user_idx)
+        //     VALUES(?,?,?)
+        // ";
+
+       $query = $this->getInsertQuery("board",(object)[
+        "title"=> $title,
+        "content"=> $content,
+        "user_idx"=> $user_idx
+        ]);
+        $this->db->query($query,array($title,$content,$user_idx));
+
+    }
+
+
+    //게시글 단일 검색
+    public function get_one($idx){
+        $query_text = "
+            SELECT
+                b.*,
+                u.name AS author
+            FROM
+                board AS b
+                LEFT JOIN user AS u
+                ON b.user_idx = u.idx
+            WHERE
+                b.idx = ?
+        ";
+         $query = $this->db->query($query_text,array($idx));
+        return $query->row();
+    }
+
+    //게시글 수정
+    public function update($title,$content,$idx){
+        $update_board = "
+                UPDATE
+                    board
+                SET
+                    title = ?,
+                    content = ?
+                WHERE
+                    idx = ?
+            ";
+        //추후 적용
+        // $update_board.= $this->getUpdateQuery('board',array(
+        //     'title'=> $title,
+        //     'content'=> $content
+        // ),(object)[
+        //     'idx' => $idzx
+        // ]);
+            $this->db->query($update_board,array($title,$content,$idx));
+
+    }
+
+    
+    //게시글 삭제
+    public function delete($idx){
+        $delete_board = "
+            DELETE FROM board
+            WHERE idx = ?
+        ";
+        $this->db->query($delete_board,array($idx));
+    }
+
+    
+
+    
+}
+
+?>
