24029a89c2bb4c347929e10151d31c3e4feb5e83
yeramdh-del <yearm.dh@gmail.com>
2025-10-17 16:05:33 +0900

    chore: 게시글 등록시 카테고리 입력란 추가

--- application/views/board/register.php
+++ application/views/board/register.php
@@ -1,7 +1,22 @@
+<?php 
+
+?>
 <div class="post_box">
-    <h2>게시글 등록</h2>
-    
+
+    <div>
+        <h2>게시글 등록</h2>
+    </div>
     <form action="/board/create" method="post">
+        <div class="form-group"  style=" display:flex; justify-content: space-between; padding: 10px 0px;">
+            <label for="category_idx" style="margin-bottom:0px;">카테고리</label>
+            
+            <select id="category_idx" name="category_idx">
+                <?php foreach($categorys as $category) : ?>
+                    <option value="<?= $category->idx?>"><?= $category->title?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+
         <div class="form-group">
             <label for="title">제목</label>
             <input type="text" id="title" name="title" required placeholder="게시글 제목을 입력하세요">
@@ -18,3 +33,9 @@
         </div>
     </form>
 </div>
+
+<style>
+
+
+
+</style>
