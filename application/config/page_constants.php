c8c4b73f7125b2644488c3cf096f27a47eb389ce
yeramdh-del <yearm.dh@gmail.com>
2025-10-22 13:06:37 +0900

    feat: 페이지 렌더링 URL경로 상수 저장 파일 추가

---
+++ application/config/page_constants.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+// NOTE: 페이지 렌더링시 경로 상수값 지정
+$config['view_names'] = [
+    'BOARD_LIST' => 'board/list',
+    'BOARD_REGISTER' => 'board/register',
+    'BOARD_EDIT'=> 'board/edit',
+    'BOARD_DETAIL' => 'board/detail',
+    'AUTH_LOGIN' => 'auth/login',
+    'AUTH_REGISTER' => 'auth/register', 
+    
+
+];
+
+$config['page_titles'] = [
+    'BOARD_LIST' => '게시판 목록',
+    'BOARD_REGISTER' => '게시글 등록',
+    'BOARD_EDIT' => '게시글 수정',
+    'BOARD_DETAIL' => '게시글 상세페이지',
+    'AUTH_LOGIN' => '로그인',
+    'AUTH_REGISTER' => '회원가입'
+
+];
